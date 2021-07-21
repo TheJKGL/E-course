6cb8f9dd3bfbd92ffc1618992edabe662ff44e65
Yevhenii Malakhov <malakhov747@gmail.com>
2021-07-21 17:39:37 +0300

    MyCollection version02, add Task2

--- MyCollection/src/main/java/Task1.java
+++ MyCollection/src/main/java/Task1.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.stream.Stream;
 
 public class Task1 {
     public static void main(String[] args) {
